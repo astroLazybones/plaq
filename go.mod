a3c79b6c266ce709d1f949c9edd5801f615c3425
Astro <148036040+astroLazybones@users.noreply.github.com>
2024-06-22 00:21:37 +1200

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/astrolazybones/plaq
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+import (
+   //"io/ioutil"
+   //"log"
+   //"net/http"
+   //"container/list"
+)
+
+var Reset = "\033[0m" 
+var Red = "\033[31m" 
+var Green = "\033[32m" 
+var Yellow = "\033[33m" 
+var Blue = "\033[34m" 
+var Magenta = "\033[35m" 
+var Cyan = "\033[36m" 
+var Gray = "\033[37m" 
+var White = "\033[97m"
+
+var yes = "y"
+var no = "n"
+var dflt = yes
+var dw_dir = "./.plaqs"
+
+func main() {
+	askconfirm()
+}
+
+func askconfirm() {
+	for {
+		fmt.Println("[" + Green + yes + Reset + "/" + Red + no + Reset + "]")
+		var confi string
+		fmt.Scanln(&confi)
+		if len(confi) == 0 {
+			fmt.Println("womp womp")
+		} else {
+			break
+		}
+	}
+}
